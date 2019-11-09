234c32eefd514356e9404b3868c19eec7df469c2
Th5tch3r <amber.phan27@gmail.com>
2019-11-09 13:00:45 -0500

    adding MOTD to server using async and await

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const helmet = require('helmet');
+
+const db = require("./data/dbConfig");
 
 const server = express();
 
 const actionsRouter = require("./data/routers/actionsRouter");
 const projectsRouter = require("./data/routers/projectsRouter");
 
+server.use(helmet())
 server.use(express.json());
 // server.use(logger);
 server.use("/actions", actionsRouter);
 server.use("/projects", projectsRouter);
 
-server.get("/", (req, res) => {
-    res.status(200).json({message: "WELCOME TO THATCHER'S SPRINT"})
+server.get("/", async (req, res) => {
+    try {
+        const info = await db('info');
+        const motd = process.env.MOTD || "HELLO!";
+        res.status(200).json({message: motd, info})
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Cannot retrieve the info!'})
+    }
 });
 
 function logger(req, res, next) {
